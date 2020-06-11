b1a40b07523c39785b2dc62567dc27bac074cec8
Deepak Senguttuvan <Deepak.Senguttuvan@tsi.com>
2020-06-11 15:35:58 +0530

    cors added

--- app.js
+++ app.js
@@ -14,12 +14,6 @@ var cors = require('cors');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://computeengineondemand.appspot.com/turn?username=41784574&key=4080218913");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -29,7 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://dmeet.herokuapp.com/'
+}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
