8403298defd174a42ce5658d1fa44e5e646a31b7
Deepak Senguttuvan <Deepak.Senguttuvan@tsi.com>
2020-06-11 17:15:57 +0530

    Added cors options

--- app.js
+++ app.js
@@ -23,9 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.use(cors({
-  origin: 'https://dmeet.herokuapp.com/'
-}));
+app.options('*', cors())
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
