41cf605da73ffaa0cbf75f8f541ed6399c0b095d
Deepak Senguttuvan <Deepak.Senguttuvan@tsi.com>
2020-06-11 17:32:50 +0530

    changed cors origin

--- app.js
+++ app.js
@@ -23,7 +23,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.options('*', cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://dmeet.herokuapp.com/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
